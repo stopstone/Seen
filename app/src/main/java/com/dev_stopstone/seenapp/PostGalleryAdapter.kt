68c9c23239435980d35e04de6721a96b3464982b
StopStone <wltjr6432@naver.com>
2024-03-09 18:54:37 +0900

    feat: 분실물 등록 이미지 여려 장 추가 기능

---
+++ app/src/main/java/com/dev_stopstone/seenapp/PostGalleryAdapter.kt
@@ -0,0 +1,46 @@
+package com.dev_stopstone.seenapp
+
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.dev_stopstone.seenapp.databinding.ItemRegisterPhotoBinding
+
+class PostGalleryAdapter(
+    private val items: ArrayList<Uri>,
+) : RecyclerView.Adapter<PostGalleryAdapter.PostGalleryViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostGalleryViewHolder {
+        return PostGalleryViewHolder.from(parent)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: PostGalleryViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    class PostGalleryViewHolder(private val binding: ItemRegisterPhotoBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(imageView: Uri) {
+            Glide.with(binding.root)
+                .load(imageView)
+                .into(binding.ivRegisterPhoto)
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): PostGalleryViewHolder {
+                val binding = ItemRegisterPhotoBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+                return PostGalleryViewHolder(binding)
+            }
+        }
+
+    }
+
+}
