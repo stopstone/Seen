e4ae1f7ef1e4543b9191579d2885949256d7c192
StopStone <wltjr6432@naver.com>
2024-04-03 00:28:15 +0900

    build: safeargs 종속성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
+    id("androidx.navigation.safeargs.kotlin")
     id("kotlin-parcelize")
-    id("androidx.navigation.safeargs")
 }
 
 android {
