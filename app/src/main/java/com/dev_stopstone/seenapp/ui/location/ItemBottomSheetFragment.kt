0372ca79993f044e75adb3c2cee0da825720f9da
StopStone <wltjr6432@naver.com>
2024-04-17 20:32:32 +0900

    refactor: 불필요한 apply 함수 제거

--- app/src/main/java/com/dev_stopstone/seenapp/ui/location/ItemBottomSheetFragment.kt
+++ app/src/main/java/com/dev_stopstone/seenapp/ui/location/ItemBottomSheetFragment.kt
@@ -4,21 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.dev_stopstone.seenapp.data.LostItem
 import com.dev_stopstone.seenapp.databinding.FragmentItemBottomSheetBinding
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.google.firebase.Firebase
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.database
 
 class ItemBottomSheetFragment : BottomSheetDialogFragment() {
     private var _binding: FragmentItemBottomSheetBinding? = null
     private val binding get() = _binding!!
-
-    private lateinit var database: FirebaseDatabase
-    private val items = mutableListOf<LostItem>()
-    private val adapter = LocationItemAdapter(items)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,36 +22,12 @@ class ItemBottomSheetFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        database = Firebase.database
 
-        val behavior = BottomSheetBehavior.from(view.parent as View)
-        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        TODO("Bottom Sheet에서 주변 분실물만 가져와서 보여주기")
+    }
 
-        // 상태 변화 리스너 추가
-        behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
-            override fun onStateChanged(bottomSheet: View, newState: Int) {
-                when (newState) {
-                    BottomSheetBehavior.STATE_DRAGGING -> {
-                        // 드래그 중일 때 동작
-                    }
-
-                    BottomSheetBehavior.STATE_EXPANDED -> {
-                        // 완전히 펼쳐졌을 때 동작
-                    }
-
-                    BottomSheetBehavior.STATE_COLLAPSED -> {
-                        // 완전히 접혔을 때 동작
-                    }
-
-                    BottomSheetBehavior.STATE_HIDDEN -> {
-                        // 숨겨졌을 때 동작
-                    }
-                }
-            }
-
-            override fun onSlide(bottomSheet: View, slideOffset: Float) {
-                // 슬라이딩 중일 때 동작
-            }
-        })
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
