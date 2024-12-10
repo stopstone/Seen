fff51fa01bb561206ef5aed5c2230f763073e815
StopStone <wltjr6432@naver.com>
2024-12-10 10:22:38 +0900

    refactor: 스플래쉬 Handler에서 코루틴으로 변경

--- app/src/main/java/com/dev_stopstone/seenapp/ui/SplashActivity.kt
+++ app/src/main/java/com/dev_stopstone/seenapp/ui/SplashActivity.kt
@@ -5,11 +5,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.dev_stopstone.seenapp.MainActivity
 import com.dev_stopstone.seenapp.R
 import com.dev_stopstone.seenapp.ui.profile.LoginActivity
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
@@ -18,14 +23,17 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            intent = if (auth.currentUser != null) {
-                Intent(this, MainActivity::class.java)
-            } else {
-                Intent(this, LoginActivity::class.java)
+        intent = when(auth.currentUser) {
+            null -> Intent(this, LoginActivity::class.java)
+            else -> Intent(this, MainActivity::class.java)
+        }
+
+        lifecycleScope.launch {
+            delay(2000)
+            withContext(Dispatchers.Main) {
+                startActivity(intent)
+                finish()
             }
-            startActivity(intent)
-            finish()
-        }, 1000)
+        }
     }
 }
