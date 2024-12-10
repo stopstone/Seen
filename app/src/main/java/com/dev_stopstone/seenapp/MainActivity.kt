6b25d3f9f24e27431bf743a6d565ecd474848c24
StopStone <wltjr6432@naver.com>
2024-12-10 10:23:18 +0900

    refactor: RequiresApi 제거

--- app/src/main/java/com/dev_stopstone/seenapp/MainActivity.kt
+++ app/src/main/java/com/dev_stopstone/seenapp/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.dev_stopstone.seenapp
 
-import android.os.Build
 import android.os.Bundle
 import android.view.View.GONE
 import android.view.View.VISIBLE
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -13,7 +11,6 @@ import com.dev_stopstone.seenapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
