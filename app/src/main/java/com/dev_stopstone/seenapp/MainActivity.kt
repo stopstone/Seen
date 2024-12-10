bb363ad3828b803a266bad9fb15961c6aa538223
StopStone <wltjr6432@naver.com>
2024-12-10 10:23:59 +0900

    refactor: Binding 객체 by lazy로 초기화

--- app/src/main/java/com/dev_stopstone/seenapp/MainActivity.kt
+++ app/src/main/java/com/dev_stopstone/seenapp/MainActivity.kt
@@ -9,11 +9,10 @@ import androidx.navigation.ui.setupWithNavController
 import com.dev_stopstone.seenapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navHostFragment =
